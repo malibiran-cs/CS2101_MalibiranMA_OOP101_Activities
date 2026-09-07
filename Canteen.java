f25bf00600f4c604ed64816dc481fb4af9abcaeb
malibiran-cs <markangelomalibiran4@gmail.com>
2026-09-07 21:34:00 +0800

    Initialized scanner system

--- Canteen.java
+++ Canteen.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 class Canteen {
     public static void main(String[] args) {
     String[] foodMenu = {"Burger", "Pizza", "Bacon Egg and Cheese Sandwich", "Nachos", "Fish Fillet"};
@@ -6,5 +7,20 @@ class Canteen {
     System.out.println("=====   M E N U   =====");
     for (int i = 0; i < foodMenu.length; i++) {
         System.out.println((i + 1) + ". " + foodMenu[i] + " - $" + foodPrices[i]);    }
+
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("Enter item number: ");
+    int foodItem = sc.nextInt();
+
+    System.out.println("Enter quantity: ");
+    int quantity = sc.nextInt();
+
+    System.out.println("Are you a student? (Y/N)");
+    String studentStatus = sc.next();
+
+    boolean isStudent = studentStatus.equalsIgnoreCase("Y");
+
+
     }
 }
