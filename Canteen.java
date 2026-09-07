2d0215e4442b94793096b534ae5ed677556a9953
malibiran-cs <markangelomalibiran4@gmail.com>
2026-09-07 21:20:43 +0800

    Created Canteen filename, initialized menu and prize using two parallel array

---
+++ Canteen.java
@@ -0,0 +1,10 @@
+class Canteen {
+    public static void main(String[] args) {
+    String[] foodMenu = {"Burger", "Pizza", "Bacon Egg and Cheese Sandwich", "Nachos", "Fish Fillet"};
+    double[] foodPrices = {10.00, 15.00, 25.00, 15.00, 20.00};
+
+    System.out.println("=====   M E N U   =====");
+    for (int i = 0; i < foodMenu.length; i++) {
+        System.out.println((i + 1) + ". " + foodMenu[i] + " - $" + foodPrices[i]);    }
+    }
+}
